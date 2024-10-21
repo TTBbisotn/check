0c347d5665bd0369a41cf445eda5900415791036
TTBbisotn <social.ttb2018@gmail.com>
2024-10-21 10:57:23 +0330

    Update check.py

--- check.py
+++ check.py
@@ -1,22 +1,22 @@
 import re
 from time import sleep
+from duplicate_file_remover import remove_duplicate_lines
 
-inputfile = open('story1data.txt','r',encoding='utf-8')
-
-outputfile = open('export/story1data.txt','w+',encoding='utf-8')
+inputfile = open('story2data.txt','r',encoding='utf-8')
 
 
-regex = r"(?:\'s profile picture\n(.*)|dateTime (.*)\.)"
+outputfile = open('export/story2data.txt','w+',encoding='utf-8')
+
+regex = r"(?:\'s profile picture\n(.*)|(\d+\-\d+\-\d+ \d+:\d+:\d+))"
 # data = re.findall('\'s profile picture\n(.*)',inputfile.read())
 # data = re.findall('\d+\-\d+\-\d+ \d+:\d+:\d+',inputfile.read())
 data = re.findall(regex,inputfile.read(),re.MULTILINE)
 
 # for i in data:
-#     print(data)
-
+#     outputfile.write(i+';\n')
 for i in data:
     for a in i :
         if a == '':
             pass
         else:
-            outputfile.write(a+";\n")
+            outputfile.write(a+';\n')
